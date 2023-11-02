0d37ddf6e007046c198a42b0f353df423a2059ea
Luis Amaison <luis.unlam@gmail.com>
2023-11-02 14:25:27 -0300

    Desafio 01: Clases con ECMAScript

---
+++ desafio_01_clases.js
@@ -0,0 +1,34 @@
+//Declaro la clase ProductManager con sus metodos
+class ProductManager {
+
+    constructor(){
+        this.pruducts=[];
+        this.counter=0;   
+    }
+
+    addProduct(){
+
+    }
+    getProduct(){
+
+    }
+    getProductById(){
+        
+    }
+}
+
+//Declaro la clase Product
+class Product{
+
+    constructor(title, description, price, thumbnail, code, stock){
+
+        this.title = title;
+        this.description = description;
+        this.price = price;
+        this.thumbnail = thumbnail;
+        this.code = code;
+        this.stock = stock;
+    }
+        
+
+}
