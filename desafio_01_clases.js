9cc32f581f0ec9342da2a3b7f867140c86d535ce
Luis Amaison <luis.unlam@gmail.com>
2023-11-06 10:31:22 -0300

    Desafio 01: Clases con ECMAScript

--- desafio_01_clases.js
+++ desafio_01_clases.js
@@ -1,38 +1,40 @@
 //Declaro la clase ProductManager con sus metodos
 class ProductManager {
 
-    constructor(){
-        this.pruducts=[];
-        this.counter=1;   
+    constructor() {
+        this.products = [];
     }
 
-    addProduct(product){
-        const codeProduct = this.products.find((product) => product.code === products.code);
+    addProduct(product) {
+        const codeProduct = this.products.find((products) => products.code === products.code);
 
-        if(codeProduct){
-            console.log("This code already exists");
+        if (codeProduct) {
+            console.log("********[]-This code already exists-[]********");
             return;
-        }else{
-            product.id = this.counter;
-            this.counter ++;
+        } else {
+            product.code = this.products[this.products.length - 1].code + 1;
             this.products.push(product)
         }
 
     }
-    getProduct(){
+    getProducts() {
         console.log(this.products);
     }
-    getProductById(code){
-        let findId = this.products.find((product) => product.id === id);
+    getProductById(code) {
+        let findId = this.products.find((product) => product.code === code);
+
+        if(findId){
+            let productFoud =
+        }
         console.log(findId);
         return findId;
     }
 }
 
 //Declaro la clase Product
-class Product{
+class Product {
 
-    constructor(title, description, price, thumbnail, code, stock){
+    constructor(title, description, price, thumbnail, code, stock) {
 
         this.title = title;
         this.description = description;
@@ -41,6 +43,18 @@ class Product{
         this.code = code;
         this.stock = stock;
     }
-        
-
 }
+
+const productManager = new ProductManager();
+
+productManager.getProducts();
+
+productManager.addProduct( new Product("producto de prueba", "esto es un producto de prueba", 200, "Sin imagen", 0, 25));
+
+productManager.getProducts();
+
+productManager.addProduct( new Product("producto de prueba", "esto es un producto de prueba", 200, "Sin imagen", 1, 25));
+
+productManager.getProductById(1)
+
+productManager.getProductById(2)
